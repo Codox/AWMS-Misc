bc7a207d84a82539e524da97d25466713a3287fe
Connor Brady <52251127+rexchoppers@users.noreply.github.com>
2023-05-15 08:05:51 +0100

    Create k8s-dev-server-commands.sh

---
+++ k8s-dev-server-commands.sh
@@ -0,0 +1,12 @@
+microk8s enable dns dashboard
+sudo apt install snapd
+sudo snap install core
+sudo snap install microk8s --classic
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+sudo snap install microk8s --classic
+sudo snap install microk8s --classic --channel=1.27
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+microk8s kubectl -n kube-system edit service kubernetes-dashboard
+microk8s kubectl -n kube-system get services
